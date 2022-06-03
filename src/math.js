e4f737bdbf9277fe7d60c35b347d3e8859f43498
Valerrriy Orlov <valerrriyorlov@yandex.ru>
2022-06-03 15:59:44 +0300

    fix math.js

--- src/math.js
+++ src/math.js
@@ -20,18 +20,17 @@ export const getOperation = () => {
 export const calc = (operand1, operand2, operation) => {
   let result = 0;
   switch (operation) {
-  case '+':
-    result = operand1 + operand2;
-    break;
-  case '-':
-    result = operand1 - operand2;
-    break;
-  case '*':
-    result = operand1 * operand2;
-    break;
-  case '/':
-    result = operand1 / operand2;
-    break;
+    case '+':
+      result = operand1 + operand2;
+      break;
+    case '-':
+      result = operand1 - operand2;
+      break;
+    case '*':
+      result = operand1 * operand2;
+      break;
+    default:
+      break;
   }
   return Math.ceil(result);
 };
