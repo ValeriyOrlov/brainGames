554b883c692514579bf6bfd7161c84e6b2efccec
Valerrriy Orlov <valerrriyorlov@yandex.ru>
2022-06-06 17:08:33 +0300

    add: brain-even-data.js

---
+++ src/games/brain-even-data.js
@@ -0,0 +1,11 @@
+import { getRandom, isEven } from "../math.js";
+
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+const resultAndCorrectAnswer = () => {
+    const question = getRandom(0, 100);
+    const correctAnswer = isEven(result) ? 'yes' : 'no';
+    return [question, correctAnswer];
+};
+
+export {rule, resultAndCorrectAnswer };
