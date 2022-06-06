57429bf4f56ac088d3f516b385cd4fd608a1a1bc
Valerrriy Orlov <valerrriyorlov@yandex.ru>
2022-06-06 17:13:47 +0300

    feat: brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,32 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import playerName from '../src/cli.js';
-import { getRandom, isEven } from '../src/math.js';
+import { rule, resultAndCorrectAnswer } from '../src/games/brain-even-data.js';
+import letsPlay from '../src/index.js';
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let winCounter = 0;
-
-for (let i = 0; i < 3; i += 1) {
-  const randomNumber = getRandom(1, 100);
-  console.log(`Question: ${randomNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-
-  if (isEven(randomNumber) === true && answer === 'yes') {
-    console.log('Correct!');
-  } else if (isEven(randomNumber) === true && answer !== 'yes') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${playerName}!`);
-    break;
-  }
-
-  if (isEven(randomNumber) === false && answer === 'no') {
-    console.log('Correct!');
-  } else if (isEven(randomNumber) === false && answer !== 'no') {
-    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${playerName}!`);
-    break;
-  }
-  winCounter += 1;
-}
-
-if (winCounter === 3) {
-  console.log(`Congratulations, ${playerName}!`);
-}
+letsPlay(rule, resultAndCorrectAnswer);
