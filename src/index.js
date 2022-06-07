b2c13b27942c514c25a622e3beb80a8e2e923f8a
Valerrriy Orlov <valerrriyorlov@yandex.ru>
2022-06-07 03:22:53 +0300

    fix: added condition for game-prime

--- src/index.js
+++ src/index.js
@@ -11,10 +11,14 @@ const letsPlay = (rule, resultAndCorrectAnswer) => {
     const [question, correctAnswer] = resultAndCorrectAnswer();
     const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
 
-    if (correctAnswer !== userAnswer) {
+    if (correctAnswer !== userAnswer && rule === 'Answer "yes" if given number is prime. Otherwise answer "no".') {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    } else if (correctAnswer === userAnswer) {
+      console.log('Correct!');
+    } else {
       return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${getUserName}!`);
     }
-    console.log('Correct!');
+    
   }
   return console.log(`Congratulations, ${getUserName}!`);
 };
